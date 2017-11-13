eedb4402fed9d9c92bc2d0ccf9b846bf3f07ea04
esteban1234 <tva_libra@hotmail.com>
2017-11-13 14:06:01 -0600

    metas

--- vistas/encabezado.php
+++ vistas/encabezado.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="I_WwvHnVa0dcBKGJXE4vHeCENQL13_Q7nY9wLQ_Uya4" />
 	<link rel="icon" type="image/png" href="img/favicon.png" />
 	<title>SOVO TRANSPORTADORA Y LOGISTICA</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
